e90e40aa79a88a960db8b29f0cd3b7b48f7bf6f2
Ozan GUL <ozan.gul@outlook.com.tr>
2025-04-09 11:48:47 -0400

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+// your code goes here
+        System.out.println("ShoppingList: ");
+        System.out.println("Apple");
+        System.out.println("Bread");
+        System.out.println("Chocolate");
+        System.out.println("Tomato");
+        System.out.println("Onion");
+        System.out.println("Meat");
+        System.out.println("Milk");
+        System.out.println("Orange");
+        System.out.println("Pasta");
+        System.out.println("Rice");
+
+
+
+
+    }
+}
